f36963f3a76acb150e5347f69e698cd3589be411
Alberto Alonso <rydencillo@gmail.com>
2011-04-23 01:27:48 +0200

    Ejemplo más demostrativo para Android.

--- Android/src/es/lautech/slackgine/test/SlackgineTest.java
+++ Android/src/es/lautech/slackgine/test/SlackgineTest.java
@@ -1,8 +1,14 @@
 package es.lautech.slackgine.test;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import es.lautech.slackgine.*;
 
 public class SlackgineTest extends Activity {
@@ -10,9 +16,18 @@ public class SlackgineTest extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.main, null);
+        TextView valueTV = new TextView(this);
         Slackgine sg = new Slackgine ();
-        Log.w("Probando L3M", sg.PruebaString() );
+        valueTV.setText(sg.PruebaString());
+        valueTV.setId(5);
+        valueTV.setLayoutParams(new LayoutParams(
+                LayoutParams.FILL_PARENT,
+                LayoutParams.WRAP_CONTENT));
+
+        ((LinearLayout) view).addView(valueTV);
+        
+        setContentView(view);
     }
 }
