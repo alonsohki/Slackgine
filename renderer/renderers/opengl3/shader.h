eaceeeb9817b782318cb4f87170bf4caa7a6ad3d
Alberto Alonso <rydencillo@gmail.com>
2011-10-30 07:01:45 +0100

    License changed to LGPL.

--- renderer/renderers/opengl3/shader.h
+++ renderer/renderers/opengl3/shader.h
@@ -1,16 +1,9 @@
 /////////////////////////////////////////////////////////////
 //
-// Slackgine - Copyright (C) 2010-2011 - Alberto Alonso
+// Slackgine - Copyright (C) 2010-2011
+// The Slackgine development team
 //
-// Redistribution of this software, including any of its
-// files, in either binary or source code form is totally
-// forbidden without the permission of its owners.
-//
-// Owners reserve their right to legal action against any
-// person or company that fails to do so.
-//
-// For more detailed information, see the LICENSE file in
-// the top-level directory.
+// See the LICENSE file in the top-level directory.
 //
 // FILE:        shader.h
 // PURPOSE:     OpenGL3 shaders.
