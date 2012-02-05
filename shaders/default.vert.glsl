a4a85629ab7fccf83e9f5d2a79c6660bfee70a73
Alberto Alonso <rydencillo@gmail.com>
2012-02-06 00:24:09 +0100

    Apply the skinning to the normals as well.

--- shaders/default.vert.glsl
+++ shaders/default.vert.glsl
@@ -21,24 +21,32 @@ varying vec3 ex_Color;
 void main(void)
 {
     vec4 in_Position4 = vec4 ( in_Position, 1.0 );
+    vec4 in_Normal4 = vec4 ( in_Normal, 0.0 );
 
     ex_Color = vec3 ( in_VertexWeight + in_Joint, 0.0 );
     if ( un_Skinning == true )
     {
         float totalWeight = 0.0f;
         vec4 pos = vec4 ( 0.0, 0.0, 0.0, 1.0 );
+        vec4 norm = vec4 ( 0.0, 0.0, 0.0, 0.0 );
         for ( int i = 0; i < 2; i++ )
         {
-            pos += ( un_JointMatrices[int(in_Joint[i])] * in_Position4 ) * in_VertexWeight[i];
+            int jointID = int(in_Joint[i]);
+            pos += ( un_JointMatrices[jointID] * in_Position4 ) * in_VertexWeight[i];
+            norm += ( un_JointMatrices[jointID] * in_Normal4 ) * in_VertexWeight[i];
             totalWeight += in_VertexWeight[i];
         }
 
         pos += ( 1.0 - totalWeight ) * in_Position4;
+        norm += ( 1.0 - totalWeight ) * in_Normal4;
         gl_Position = un_Matrix * vec4(pos.xyz, 1.0);
+        ex_Normal = (un_NormalMatrix * norm).xyz;
     }
     else
+    {
         gl_Position = un_Matrix * in_Position4 * 0.0;
+        ex_Normal = (un_NormalMatrix * in_Normal4).xyz;
+    }
 
-    ex_Normal = (un_NormalMatrix * vec4(in_Normal, 1.0)).xyz;
     ex_TexCoord = in_TexCoord;
 }
