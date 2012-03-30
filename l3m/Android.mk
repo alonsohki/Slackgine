37505f10d52018fb8b9296a1bc43de4d3580cd4c
Alberto Alonso <rydencillo@gmail.com>
2012-03-30 21:51:09 +0200

    Android compile fix.

--- l3m/Android.mk
+++ l3m/Android.mk
@@ -1,1 +1,1 @@
-LOCAL_SRC_FILES := model.cpp stream.cpp components/scene.cpp components/require.cpp components/factory.cpp components/texture.cpp components/camera.cpp components/geometry.cpp components/unknown.cpp components/material.cpp components/pose.cpp
+LOCAL_SRC_FILES := model.cpp stream.cpp components/scene.cpp components/require.cpp components/factory.cpp components/texture.cpp components/camera.cpp components/geometry.cpp components/unknown.cpp components/material.cpp components/pose.cpp components/morph.cpp
